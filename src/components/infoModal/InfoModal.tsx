2f5a5b63cf557891af661fccd62ed7ada5eb81b8
smyler3 <mylertalym01@gmail.com>
2025-11-27 00:32:21 +1100

    feat: add info modal content

--- src/components/infoModal/InfoModal.tsx
+++ src/components/infoModal/InfoModal.tsx
@@ -1,7 +1,48 @@
-export default function InfoModal() {
+import styles from "./InfoModal.module.css";
+
+type InfoModalProps = {
+    handlePlayButton: () => void,
+};
+
+export default function InfoModal({ handlePlayButton: handlePlayButton}: InfoModalProps) {
     return (
-        <>
-            <h2>How to play</h2>
-        </>
+        <div className={styles.infoModal}>
+            <h2 className={styles.modalHeading}>How to play</h2>
+            <p className={styles.tagline}>Find the 5 highest-scoring words in 10 minutes</p>
+            <section className={styles.hintSection}>
+                <h3>Building Words:</h3>
+                <ol className={styles.hintList}>
+                    <li>3+ letters</li>
+                    <li>Adjacent letters only (horizontal, vertical, or diagonal)</li>
+                    <li>Each letter used once per word</li>
+                </ol>
+                <img src="" alt="" className={styles.image} />
+            </section>
+            <section className={styles.hintSection}>
+                <h3>Scoring:</h3>
+                <ol className={styles.hintList}>
+                    <li>Each letter has a value</li>
+                    <li>A word's score = the sum of it's letters</li>
+                    <li>Your final score = your best 5 words only</li>
+                </ol>
+                <img src="" alt="" className={styles.image} />
+            </section>
+            <section className={styles.hintSection}>
+                <h3>Hints:</h3>
+                <ol className={styles.hintList}>
+                    <li>Every word you find adds its score toward earning hints</li>
+                    <li>Every 20 total points = 1 hint</li>
+                    <li>A hint lets you reveal one letter from a lowest-scoring top-5 word of your choice</li>
+                </ol>
+                <img src="" alt="" className={styles.image} />
+            </section>
+            <button 
+                className={styles.playButton}
+                onClick={handlePlayButton}
+            >
+                Play
+            </button>
+            <p className={styles.credit}>Inspired by the amazing <a href="" className={styles.creditLink}>Chiddle</a></p>
+        </div>
     )
 };
--- src/components/modalPortal/ModalPortal.tsx
+++ src/components/modalPortal/ModalPortal.tsx
@@ -3,6 +3,7 @@ import HintModal from "../hintModal/HintModal";
 import InfoModal from "../infoModal/InfoModal";
 import ResultsModal from "../resultsModal/ResultsModal";
 import styles from "./ModalPortal.module.css";
+import closeIcon from "/closeIcon.svg";
 
 interface ModalPortalProps {
     modalInfo: { 
@@ -18,21 +19,23 @@ export default function ModalPortal({ modalInfo, handleCloseModal }: ModalPortal
     const shouldShowResultsModal = modalInfo.modalToShow === "resultsModal";
 
     return (
+        modalInfo.shouldShowModal && (
         <div className={styles.modalBackground}>
             <dialog className={styles.modalContainer}>
-                    <Activity mode={shouldShowInfoModal ? "visible" : "hidden"} >
-                        <InfoModal />
-                    </Activity>
-                    <Activity mode={shouldShowHintModal ? "visible" : "hidden"} >
-                        <HintModal />
-                    </Activity>
-                    <Activity mode={shouldShowResultsModal ? "visible" : "hidden"} >
-                        <ResultsModal />
-                    </Activity>
                 <button className={styles.closeButton} onClick={handleCloseModal}>
-                    Close
+                    <img src={closeIcon} alt="" />
                 </button>
+                <Activity mode={shouldShowInfoModal ? "visible" : "hidden"} >
+                    <InfoModal handlePlayButton={handleCloseModal} />
+                </Activity>
+                <Activity mode={shouldShowHintModal ? "visible" : "hidden"} >
+                    <HintModal />
+                </Activity>
+                <Activity mode={shouldShowResultsModal ? "visible" : "hidden"} >
+                    <ResultsModal />
+                </Activity>
             </dialog>
         </div>
+        )
     )
 };
