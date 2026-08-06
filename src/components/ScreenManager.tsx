d9ffc56f7ce6f3399f7712037ca1de9d2292d3c1
smyler3 <mylertalym01@gmail.com>
2026-08-06 17:55:07 +1000

    feat: add additional save scenarios

--- src/components/ScreenManager.tsx
+++ src/components/ScreenManager.tsx
@@ -1,11 +1,22 @@
-import Footer from "./footer/Footer"
-import Header from "./header/Header"
-import Home from "./home/Home"
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import Home from "./home/Home";
 import { useScreen } from "../hooks/setScreen/useScreen";
 import GameBody from "./gameBody/GameBody";
+import { useGameState } from "../hooks/gameState/useGameState";
+import { saveGame } from "../utils/saveGame";
+import { useEffect } from "react";
+import { useFetchedData } from "../hooks/fetchedData/useFetchedData";
 
 export default function ScreenManager() {
+    const { apiVersion } = useFetchedData();
     const { shouldShowGame } = useScreen();
+    const gameState = useGameState();
+
+    // When the user switches screens their progress will be saved
+    useEffect(() => {
+        saveGame({ gameState, apiVersion });
+    }, [shouldShowGame, gameState, apiVersion]);
 
     if (shouldShowGame) {
         return (
@@ -14,10 +25,8 @@ export default function ScreenManager() {
                 <GameBody />
                 <Footer />
             </>
-        )
-    };
+        );
+    }
 
-    return (
-        <Home /> 
-    )
+    return <Home />;
 }
